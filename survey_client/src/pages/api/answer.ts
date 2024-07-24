5ac3302e391bc3902737acdbef3c23922e71ff78
Ryan Ren <penghaoren7@gmail.com>
2024-07-24 14:16:33 +1000

    Create mock api, integrated frontend and backend

--- survey_client/src/pages/api/answer.ts
+++ survey_client/src/pages/api/answer.ts
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { postAnswer } from "@/service/answer";
 
 function genAnswerInfo(reqBody: any) {
   let answerList: any[] = [];
@@ -16,14 +17,22 @@ function genAnswerInfo(reqBody: any) {
   };
 }
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method !== "POST") {
     res.status(200).json({ errno: -1, msg: "method not allowed" });
   }
   const answerInfo = genAnswerInfo(req.body);
   console.log("answerInfo", answerInfo);
   try {
-    res.redirect("/Success");
+    const resData = await postAnswer(answerInfo);
+    if (resData.errno === 0) {
+      res.redirect("/Success");
+    } else {
+      res.redirect("/Fail");
+    }
   } catch (e) {
     res.redirect("/Fail");
   }
---
+++ survey_client/src/service/ajax.ts
@@ -0,0 +1,17 @@
+const HOST = "http://localhost:3001";
+
+export async function get(url: string) {
+  const res = await fetch(`${HOST}${url}`);
+  return res.json();
+}
+
+export async function post(url: string, body: any) {
+  const res = await fetch(`${HOST}${url}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+  return res.json();
+}
