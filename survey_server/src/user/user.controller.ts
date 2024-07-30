803f50ee43fd0fff0f0337cf127e614e8e23c515
Ryan Ren <penghaoren7@gmail.com>
2024-07-30 16:04:17 +1000

    Redirect user's req to auth

--- survey_server/src/user/user.controller.ts
+++ survey_server/src/user/user.controller.ts
@@ -1,17 +1,21 @@
 import {
   Body,
   Controller,
+  Get,
   HttpException,
   HttpStatus,
   Post,
+  Redirect,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { Public } from 'src/auth/decorator/public.decorator';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @Public()
   @Post('register')
   async register(@Body() userData: CreateUserDto) {
     try {
@@ -19,5 +23,18 @@ export class UserController {
     } catch (e) {
       throw new HttpException(e, HttpStatus.BAD_REQUEST);
     }
+  }
+
+  @Get('info')
+  @Redirect('/api/auth/profile', 302) //301 Permanent Redirect, 302 Temporary Redirect
+  async getUserInfo() {
+    return {};
+  }
+
+  @Public()
+  @Post('login')
+  @Redirect('/api/auth/login', 307) //307 Temporary Redirect, 308 Permanent Redirect
+  async login() {
+    return {};
   }
 }
